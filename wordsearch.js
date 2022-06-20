dcebd9210b63ead56e283ddc0baa8f0bcb916386
duducesconetto <cesconettoedu@gmail.com>
2022-06-20 04:09:51 +0000

    pair promgramming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
 const wordSearch = (letters, word) => { 
+   
+    const vertical = transpose(letters);
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
-}
+    const verticalJoin = vertical.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
+    }
+
+    return false
+  }
+
+
+
 
 module.exports = wordSearch
+
